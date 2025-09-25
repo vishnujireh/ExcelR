b1bbd4a7e0c27fe88773793c9e31f1e5e8fba5a3
vishnujireh <vishnu@jirehsol.com>
2025-09-25 12:16:59 +0530

    Initial commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+export default function Footer() {
+  return (
+    <footer className="w-full py-4 px-8 bg-gray-800 text-white text-center">© 2024 MyApp. All rights reserved.</footer>
+  );
+}
